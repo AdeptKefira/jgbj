7f401c6d7ff03626e99956fb8cb4053232671830
glafi <glafirapolkovnikova2410@gmail.com>
2024-11-27 20:04:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+
+static bool poisk(vvod) {
+	int count = 0;
+	int count_1 = 0;
+	int i = 0;
+
+	while (i > 0) {
+		if (word[i] == word[i + 1]) {
+			count++;
+			most_word = word[i];
+		}
+		else {
+			count_1++;
+		}
+		if (count_1 > count) {
+			most_word = word[i + 1];
+		}
+	}
+	return vvod;
+}
+int main() {
+	std::cout << "input file";
+	std::cout << "input N";
+	std::cout << "most word: ";
+	otv = vvod.poisk;
+	for (a = 0 : n) {
+		std::count << otv;
+	}
+	return 0;
+}
